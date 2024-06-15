ea14b7d2739959ad6b1370b4d611387d3386f7dc
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-06-15 21:29:51 +0200

    updated the javascript file

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+function predictName(response) {
+  console.log(response.data);
+}
+
+function searchName(name) {
+  let apiUrl = `https://api.agify.io?name=${name}`;
+
+  axios.get(apiUrl).then(predictName);
+}
+
 function handleFormSubmission(event) {
   event.preventDefault();
   let nameInput = document.querySelector("#name-input");
   let nameElement = document.querySelector("#name");
   nameElement.innerHTML = nameInput.value;
+  searchName(nameInput.value);
 }
 
 let submitForm = document.querySelector("#form");
