51ad30dd906ef4be022139c9dc035066e552d690
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-06-15 21:12:08 +0200

    updated the js file

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function handleFormSubmission(event) {
+  event.preventDefault();
+  let nameInput = document.querySelector("#name-input");
+  let nameElement = document.querySelector("#name");
+  nameElement.innerHTML = nameInput.value;
+}
 
+let submitForm = document.querySelector("#form");
+submitForm.addEventListener("submit", handleFormSubmission);
